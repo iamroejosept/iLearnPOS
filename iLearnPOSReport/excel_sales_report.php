34a8e5b92dd15fe1ff219f3c6323090dd8e52743
Roe Josept L. Mejia <74046150+iamroejosept@users.noreply.github.com>
2023-01-27 21:07:41 +0800

    viewmedetailedreport - done

--- iLearnPOSReport/excel_sales_report.php
+++ iLearnPOSReport/excel_sales_report.php
@@ -197,6 +197,66 @@ if(isset($_REQUEST['viewdeliveryreport'])){
     }
 }
 
+//Done
+if(isset($_REQUEST['viewmemdetailedreport'])){
+    $fileName = "viewmemdetailedreport.xls"; 
+    $excelData="Baguio-Benguet Community Credit Cooperative". "\n"; 
+    $excelData.="View Me Detailed Report". "\n"; 
+    $excelData.=$_REQUEST['datefrom']." - ".$_REQUEST['dateto']."\n\n"; 
+    // Display column names as first row 
+    // $excelData.= implode("\t", array_values($fields)) . "\n"; 
+     
+    // Fetch records from database 
+    $query = "SELECT * FROM db_sales where sales_date >= '".dashDate($_REQUEST['datefrom'])."' AND sales_date <=  '".dashDate($_REQUEST['dateto'])."' AND member_id='".$_REQUEST['memberid']."'";
+    $result = $db->query($query);
+    if($result->num_rows > 0){ 
+        // Output each row of the data 
+            while($row = $result->fetch_assoc() ){
+                $response[] = array("invoiceno"=>$row['invoiceno'],"gross_sale"=>$row['gross_sale'],"discount_amount"=>$row['discount_amount'],"station"=>$row['station'],"sales_date"=>$row['sales_date'],"sold_by"=>$row['sold_by'],"member_id"=>$row['member_id'],"payment_mode"=>$row['payment_mode'],"sales_id"=>$row['sales_id']);
+            }
+
+            foreach($response as $key => $val){
+                $invoiceamount=0;
+
+                $query1 = "SELECT * FROM db_users WHERE userid='".$val['sold_by']."'";
+                $result1 = mysqli_query($con,$query1);
+                $row1 = mysqli_fetch_array($result1); 
+
+                $excelData.= "INVOICE NUMBER:\t" .$val["invoiceno"] ."\t\tCASHIER:\t" .$row1['fname']." ".$row1['lname'] ."\n";
+                $excelData.= "INVOICE DATE:\t" .$val["sales_date"] ."\t\tTERMINAL:\t" .$val["station"] ."\n\n";
+
+                // Column names 
+                $fields = array('BARCODE', 'PRODUCT NAME', 'QTY', 'PRICE', 'ITEM DISCOUNT (%)', 'AMOUNT'); 
+                $excelData.= implode("\t", array_values($fields)) ."\n";
+
+                $result = $db->query("SELECT * FROM db_sales_product where sales_id = '".$val['sales_id']."'");
+    
+                while($row = $result->fetch_assoc()){
+                    $amount=$row['quantity']*$row['sale_price'];
+
+                    $querys = "SELECT barcode,product_name FROM db_products WHERE product_id='".$row['product_id']."'";
+                    $results = mysqli_query($con,$querys);
+                    $rows = mysqli_fetch_array($results); 
+
+                    $response1 = array("barcode"=>$rows['barcode'],"product_name"=>$rows['product_name'],"quantity"=>$row['quantity'],"sale_price"=>number_format($row['sale_price'],2),"discount"=>$row['discount'],"amount"=>number_format($amount,2));
+
+                    $invoiceamount+=$amount;
+
+                    array_walk($response1, 'filterData'); 
+                    
+                    $excelData .= implode("\t", array_values($response1)) . "\n"; 
+                }
+
+                $excelData.= "DISCOUNT:\t" .number_format($val['discount_amount'],2)."\t\t";
+                $excelData.= "INVOICE AMOUNT:\t" .number_format($invoiceamount-$val['discount_amount'],2)."\n\n";
+                
+            }
+            $excelData .= "\n";
+
+    
+    }
+}
+
 header("Content-Type: application/vnd.ms-excel"); 
 header("Content-Disposition: attachment; filename=\"$fileName\""); 
  
--- iLearnPOSReport/report.php
+++ iLearnPOSReport/report.php
@@ -28,124 +28,65 @@ function filterData(&$str){
 $month=date('F', mktime(0, 0, 0, $_REQUEST['month'], 10));
 //$fileName = "saleable.xls"; 
 
+if(isset($_REQUEST['viewmemdetailedreport'])){
+    $fileName = "viewmemdetailedreport.xls"; 
+    $excelData="Baguio-Benguet Community Credit Cooperative". "\n"; 
+    $excelData.="View Me Detailed Report". "\n"; 
+    $excelData.=$_REQUEST['datefrom']." - ".$_REQUEST['dateto']."\n\n"; 
+    // Display column names as first row 
+    // $excelData.= implode("\t", array_values($fields)) . "\n"; 
+     
+    // Fetch records from database 
+    $query = "SELECT * FROM db_sales where sales_date >= '".dashDate($_REQUEST['datefrom'])."' AND sales_date <=  '".dashDate($_REQUEST['dateto'])."' AND member_id='".$_REQUEST['memberid']."'";
+    $result = $db->query($query);
+    if($result->num_rows > 0){ 
+        // Output each row of the data 
+            while($row = $result->fetch_assoc() ){
+                $response[] = array("invoiceno"=>$row['invoiceno'],"gross_sale"=>$row['gross_sale'],"discount_amount"=>$row['discount_amount'],"station"=>$row['station'],"sales_date"=>$row['sales_date'],"sold_by"=>$row['sold_by'],"member_id"=>$row['member_id'],"payment_mode"=>$row['payment_mode'],"sales_id"=>$row['sales_id']);
+            }
 
-//generates saleable report excel
-if(isset($_REQUEST['saleableitems'])){
-$fileName = "saleable.xls"; 
-// Column names 
-$fields = array('BARCODE', 'PRODUCT NAME', 'PIECES', 'AMOUNT'); 
-$excelData="Baguio-Benguet Community Credit Cooperative". "\n"; 
-$excelData.="Saleable Items". "\n"; 
-$excelData.=$_REQUEST['datefrom']." - ".$_REQUEST['dateto']."\n\n"; 
-// Display column names as first row 
-$excelData.= implode("\t", array_values($fields)) . "\n"; 
- 
-// Fetch records from database 
-$query = $db->query("SELECT SUM(db_sales_product.quantity) AS quantity_sum,SUM(db_sales_product.sale_price) AS amount_sum, db_sales_product.product_id,db_sales_product.sale_price FROM db_sales,db_sales_product where  db_sales.sales_date >= '".dashDate($_REQUEST['datefrom'])."' AND  db_sales.sales_date <=  '".dashDate($_REQUEST['dateto'])."' AND db_sales_product.sales_id=db_sales.sales_id GROUP by db_sales_product.product_id ORDER by quantity_sum DESC"); 
-if($query->num_rows > 0){ 
-    // Output each row of the data 
-    while($row = $query->fetch_assoc()){ 
-        //$status = ($row['replenish'] == 1)?'Active':'Inactive'; 
-        $query2 = "SELECT * FROM db_products WHERE product_id='".$row['product_id']."'";
-        $result2 = mysqli_query($db,$query2);
-        $row2 = mysqli_fetch_array($result2); 
-        $lineData = array("barcode"=>$row2['barcode'],"product_name"=>$row2['product_name'],"pieces"=>$row['quantity_sum'],"amount"=>number_format($row['sale_price']*$row['quantity_sum'],2));
-        array_walk($lineData, 'filterData'); 
-        $excelData .= implode("\t", array_values($lineData)) . "\n"; 
-    } 
-}else{ 
+            foreach($response as $key => $val){
+                $invoiceamount=0;
+
+                $query1 = "SELECT * FROM db_users WHERE userid='".$val['sold_by']."'";
+                $result1 = mysqli_query($con,$query1);
+                $row1 = mysqli_fetch_array($result1); 
+
+                $excelData.= "INVOICE NUMBER:\t" .$val["invoiceno"] ."\t\tCASHIER:\t" .$row1['fname']." ".$row1['lname'] ."\n";
+                $excelData.= "INVOICE DATE:\t" .$val["sales_date"] ."\t\tTERMINAL:\t" .$val["station"] ."\n\n";
+
+                // Column names 
+                $fields = array('BARCODE', 'PRODUCT NAME', 'QTY', 'PRICE', 'ITEM DISCOUNT (%)', 'AMOUNT'); 
+                $excelData.= implode("\t", array_values($fields)) ."\n";
+
+                $result = $db->query("SELECT * FROM db_sales_product where sales_id = '".$val['sales_id']."'");
     
-    $excelData .= 'No records found...'. "\n"; 
-} 
+                while($row = $result->fetch_assoc()){
+                    $amount=$row['quantity']*$row['sale_price'];
 
-}
+                    $querys = "SELECT barcode,product_name FROM db_products WHERE product_id='".$row['product_id']."'";
+                    $results = mysqli_query($con,$querys);
+                    $rows = mysqli_fetch_array($results); 
 
-//generates view sale report excel
-if(isset($_REQUEST['viewsalesreport'])){
-$fileName = "viewsalesreport.xls"; 
-// Column names 
-$fields = array('INVOICE NUMBER', 'PURCHASE DATE', 'TIME', 'CASHIER', 'MEMBER ID', 'NAME', 'LOAN', 'CASH', 'AMOUNT'); 
-$excelData="Baguio-Benguet Community Credit Cooperative". "\n"; 
-$excelData.="Saleable Items". "\n"; 
-$excelData.=$_REQUEST['datefrom']." - ".$_REQUEST['dateto']."\n\n"; 
-// Display column names as first row 
-$excelData.= implode("\t", array_values($fields)) . "\n"; 
+                    $response1 = array("barcode"=>$rows['barcode'],"product_name"=>$rows['product_name'],"quantity"=>$row['quantity'],"sale_price"=>number_format($row['sale_price'],2),"discount"=>$row['discount'],"amount"=>number_format($amount,2));
 
-/*global $con;*/
- 
-// Fetch records from database 
-$query = "SELECT * FROM db_sales where sales_date >= '".dashDate($_REQUEST['datefrom'])."' AND sales_date <=  '".dashDate($_REQUEST['dateto'])."'";
-$result = $db->query($query);
-if($result->num_rows > 0){ 
-    // Output each row of the data 
-    while($row = $result->fetch_assoc() ){
+                    $invoiceamount+=$amount;
 
-        $response = array("invoiceno"=>$row['invoiceno'],"sales_date"=>$row['sales_date'],"station"=>$row['station'],"sold_by"=>$row['sold_by'],"member_id"=>$row['member_id'],"member_name"=>"","loan_amount"=>$row['loan_amount'],"discount_amount"=>$row['discount_amount'],"invoice_amount"=>$row['discount_amount']);
+                    array_walk($response1, 'filterData'); 
+                    
+                    $excelData .= implode("\t", array_values($response1)) . "\n"; 
+                }
 
-        array_walk($response, 'filterData'); 
-        
-        $excelData .= implode("\t", array_values($response)) . "\n"; 
-    } 
-}else{ 
-    
-    $excelData .= 'No records found...'. "\n"; 
-} 
-
-
-
-}
-
-
-//generates view detailed report excel
-if(isset($_REQUEST['viewdetailedsalesreport'])){
-$fileName = "viewdetailedsalesreport.xls"; 
-// Column names 
-$fields = array('BARCODE', 'PRODUCT NAME', 'QTY', 'PRICE', 'ITEM DISCOUNT(%)', 'AMOUNT'); 
-$excelData="Baguio-Benguet Community Credit Cooperative". "\n"; 
-$excelData.="Detailed Sales Items". "\n"; 
-$excelData.=$_REQUEST['datefrom']." - ".$_REQUEST['dateto']."\n\n"; 
-// Display column names as first row 
-// $excelData.= implode("\t", array_values($fields)) . "\n"; 
- 
-// Fetch records from database 
-$query = "SELECT * FROM db_sales where sales_date >= '".dashDate($_REQUEST['datefrom'])."' AND sales_date <=  '".dashDate($_REQUEST['dateto'])."'";
-$result = $db->query($query);
-if($result->num_rows > 0){ 
-    // Output each row of the data 
-        while($row = $result->fetch_assoc() ){
-            $response[] = array('invoiceno'=>$row['invoiceno'],"gross_sale"=>$row['gross_sale'],"discount_amount"=>$row['discount_amount'],"station"=>$row['station'],"sales_date"=>$row['sales_date'],"sold_by"=>$row['sold_by'],"member_id"=>$row['member_id'],"payment_mode"=>$row['payment_mode'],"sales_id"=>$row['sales_id']);
-        }
-
-        foreach($response as $key => $val){
-            $excelData.= "INVOICE NUMBER:\t" .$val["invoiceno"] ."\t\tCASHIER:\t" .$val["sold_by"] ."\n";
-            $excelData.= "INVOICE DATE:\t" .$val["sales_date"] ."\t\tTERMINAL:\t" .$val["station"] ."\n\n";
-            $excelData.= implode("\t", array_values($fields)) ."\n";
-
-            $id = array();
-            $id = $val["sales_id"]; 
-            
-            $result = $db->query("SELECT * FROM db_sales_product where sales_id = $id");
-    
-            while($row = $result->fetch_assoc()){
-            $response1 = array("invoiceno"=>" ","product_name"=>" ","quantity"=>$row['quantity'],"sale_price"=>$row['sale_price'],"discount"=>$row['discount'],"amount"=>($row['sale_price']*$row['quantity']),"discount_amount"=>" ","invoice_amount"=>" ");
-
-            array_walk($response1, 'filterData'); 
-            
-            $excelData .= implode("\t", array_values($response1)) . "\n"; 
+                $excelData.= "DISCOUNT:\t" .number_format($val['discount_amount'],2)."\t\t";
+                $excelData.= "INVOICE AMOUNT:\t" .number_format($invoiceamount-$val['discount_amount'],2)."\n\n";
+                
             }
             $excelData .= "\n";
-        }
-            
 
-         
-}else{ 
     
-    $excelData .= 'No records found...'. "\n"; 
-} 
-
-
-
+    }
 }
+
 
 header("Content-Type: application/vnd.ms-excel"); 
 header("Content-Disposition: attachment; filename=\"$fileName\""); 
